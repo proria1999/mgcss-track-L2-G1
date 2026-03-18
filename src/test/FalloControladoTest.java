c79908f2e79b7c95f28cf7836903cf83f1d07875
Pablo <Pablo@Pablete>
2026-03-18 22:45:19 +0100

    test: add controlled failing test

--- src/test/FalloControladoTest.java
+++ src/test/FalloControladoTest.java
@@ -1,13 +1,12 @@
-package com.mgcss.proyecto;
+package com.mgcss;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FalloControladoTest {
 
     @Test
-    void testDebeFallar() {
-        // Este fallo es deliberado para cumplir con el Paso 5 de la Sesión 3
-        fail("Fallo provocado para verificar la Integración Continua");
+    void testFalloControlado() {
+        assertEquals(1, 2); // Esto siempre falla
     }
 }
