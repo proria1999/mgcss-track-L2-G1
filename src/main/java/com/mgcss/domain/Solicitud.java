bca4de5837b580c8acc75f8837fc2bc77fc4716b
Pablo Pérez Muñoz <pablopermun05@gmail.com>
2026-03-27 13:41:16 +0100

    feat: Reglas de negocio Solicitud

--- src/main/java/com/mgcss/domain/Solicitud.java
+++ src/main/java/com/mgcss/domain/Solicitud.java
@@ -3,7 +3,32 @@ package com.mgcss.domain;
 import java.time.LocalDate;
 
 public class Solicitud {
-	private int id;
-	private EstadoSolicitud estado;
-	private LocalDate fechaCreacion;
+
+    private int id;
+    private EstadoSolicitud estado;
+    private LocalDate fechaCreacion;
+    private Tecnico tecnicoAsignado;
+
+    public Solicitud(EstadoSolicitud estado) {
+        this.estado = estado;
+        this.fechaCreacion = LocalDate.now();
+    }
+
+    public EstadoSolicitud getEstado() {
+        return estado;
+    }
+
+    public void cerrar() {
+        if (this.estado != EstadoSolicitud.EN_PROCESO) {
+            throw new IllegalStateException("No se puede cerrar si no está en proceso");
+        }
+        this.estado = EstadoSolicitud.CERRADA;
+    }
+
+    public void asignarTecnico(Tecnico tecnico) {
+        if (!tecnico.isActivo()) {
+            throw new IllegalArgumentException("El técnico no está activo");
+        }
+        this.tecnicoAsignado = tecnico;
+    }
 }
