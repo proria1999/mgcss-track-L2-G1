b895349342f92f44162b261dbb2e4105ea2faec1
Pablo Pérez Muñoz <pablopermun05@gmail.com>
2026-03-27 14:19:57 +0100

    test: añadir tests para reglas de cierre de solicitud y asignación de técnico

--- src/test/java/com/mgcss/domain/SolicitudTest.java
+++ src/test/java/com/mgcss/domain/SolicitudTest.java
@@ -34,4 +34,64 @@ class SolicitudTest {
             solicitud.asignarTecnico(inactivo);
         });
     }
+
+    @Test
+    void noSePuedeIniciarSiNoEstaAbierta() {
+        Solicitud s = new Solicitud(EstadoSolicitud.EN_PROCESO);
+
+        assertThrows(IllegalStateException.class, s::iniciarProceso);
+    }
+
+    @Test
+    void sePuedeIniciarDesdeAbierta() {
+        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
+
+        s.iniciarProceso();
+
+        assertEquals(EstadoSolicitud.EN_PROCESO, s.getEstado());
+    }
+
+    @Test
+    void estaCerradaDevuelveTrueSiCerrada() {
+        Solicitud s = new Solicitud(EstadoSolicitud.EN_PROCESO);
+        s.cerrar();
+
+        assertTrue(s.estaCerrada());
+    }
+
+    @Test
+    void estaCerradaDevuelveFalseSiNoCerrada() {
+        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
+
+        assertFalse(s.estaCerrada());
+    }
+
+    @Test
+    void tieneTecnicoAsignadoFunciona() {
+        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
+
+        assertFalse(s.tieneTecnicoAsignado());
+
+        Tecnico t = new Tecnico(true);
+        s.asignarTecnico(t);
+
+        assertTrue(s.tieneTecnicoAsignado());
+    }
+
+    @Test
+    void cerrarDosVecesFalla() {
+        Solicitud s = new Solicitud(EstadoSolicitud.EN_PROCESO);
+        s.cerrar();
+
+        assertThrows(IllegalStateException.class, s::cerrar);
+    }
+
+    @Test
+    void asignarTecnicoNuloFalla() {
+        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
+
+        assertThrows(NullPointerException.class, () -> {
+            s.asignarTecnico(null);
+        });
+    }
 }
