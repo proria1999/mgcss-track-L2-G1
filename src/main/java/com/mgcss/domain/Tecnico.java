b76708de9daf50d9840d38e54762ec237676cfe8
Pablo Pérez Muñoz <pablopermun05@gmail.com>
2026-03-27 13:42:30 +0100

    feat: Clase Tecnico

---
+++ src/main/java/com/mgcss/domain/Tecnico.java
@@ -0,0 +1,14 @@
+package com.mgcss.domain;
+
+public class Tecnico {
+
+    private boolean activo;
+
+    public Tecnico(boolean activo) {
+        this.activo = activo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+}
