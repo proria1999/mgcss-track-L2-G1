506f156f97d2c67db11c89f93bec7b146f219ab3
Pablo <pablodeborja@gmail.com>
2026-04-09 19:26:33 +0200

    fix: Eliminamos test duplicados de SolicitudTest

--- src/test/java/com/mgcss/domain/SolicitudTest.java
+++ src/test/java/com/mgcss/domain/SolicitudTest.java
@@ -94,40 +94,4 @@ class SolicitudTest {
             s.asignarTecnico(null);
         });
     }
-
-    @Test
-    void sePuedeDesasignarTecnico() {
-        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
-        Tecnico t = new Tecnico(true);
-
-        s.asignarTecnico(t);
-        s.desasignarTecnico();
-
-        assertFalse(s.tieneTecnicoAsignado());
-    }
-
-    @Test
-    void noSePuedeDesasignarSiNoHayTecnico() {
-        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
-
-        assertThrows(IllegalStateException.class, s::desasignarTecnico);
-    }
-
-    @Test
-    void sePuedeDesasignarTecnico() {
-        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
-        Tecnico t = new Tecnico(true);
-
-        s.asignarTecnico(t);
-        s.desasignarTecnico();
-
-        assertFalse(s.tieneTecnicoAsignado());
-    }
-
-    @Test
-    void noSePuedeDesasignarSiNoHayTecnico() {
-        Solicitud s = new Solicitud(EstadoSolicitud.ABIERTA);
-
-        assertThrows(IllegalStateException.class, s::desasignarTecnico);
-    }
 }
