dafa2ab703978ce34da181926412f28395060af6
Pablo <pablodeborja@gmail.com>
2026-05-18 09:11:15 +0200

    feat: añadimos el crear tecnico para swagger

--- src/main/java/com/mgcss/service/TecnicoService.java
+++ src/main/java/com/mgcss/service/TecnicoService.java
@@ -1,6 +1,10 @@
 package com.mgcss.service;
 
+import com.mgcss.domain.EspecialidadTecnico;
+import com.mgcss.domain.TipoCliente;
+import com.mgcss.infrastructure.persistence.ClienteEntity;
 import com.mgcss.infrastructure.persistence.JpaTecnicoRepository;
+import com.mgcss.infrastructure.persistence.SolicitudEntity;
 import com.mgcss.infrastructure.persistence.TecnicoEntity;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +26,17 @@ public class TecnicoService {
 
     public List<TecnicoEntity> listarTodos() {
         return jpaTecnicoRepository.findAll();
+    }
+    
+    public TecnicoEntity crearTecnico(String nombre, EspecialidadTecnico especialidad) {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new IllegalArgumentException("El nombre del tecnico no puede estar vacío");
+        }
+
+        TecnicoEntity tecnico = new TecnicoEntity();
+        tecnico.setNombre(nombre);
+        tecnico.setEspecialidad(EspecialidadTecnico.MANTENIMIENTO);
+        
+        return jpaTecnicoRepository.save(tecnico);
     }
 }
